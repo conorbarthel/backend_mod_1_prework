4cb86f4f9a32ca82219ee99cbf7b358b7fef5c11
Conor Barthel <conor.barthel@gmail.com>
2021-11-06 14:52:12 -0700

    Add section 3 exercises and reflection

--- section3/exercises/hashes.rb
+++ section3/exercises/hashes.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section3/exercises/hashes.rb`
 
 # Example: Write code that prints a hash holding grocery store inventory:
@@ -8,25 +8,26 @@ foods = {apples: 23, grapes: 507, eggs: 48}
 p foods
 
 # Write code that prints a hash holding zoo animal inventory:
-zoo = #YOUR CODE HERE
+zoo = {giraffes: 5, lions: 9, chimps: 12}
 p zoo
 
-# Write code that prints all of the 'keys' of the zoo variable 
+# Write code that prints all of the 'keys' of the zoo variable
 # you created above:
 # YOUR CODE HERE
-
-# Write code that prints all of the 'values' of the zoo variable 
+puts zoo.keys
+# Write code that prints all of the 'values' of the zoo variable
 # you created above:
 # YOUR CODE HERE
-
-# Write code that prints the value of the first animal of the zoo variable 
+puts zoo.values
+# Write code that prints the value of the first animal of the zoo variable
 # you created above:
 # YOUR CODE HERE
-
-# Write code that adds an animal to the zoo hash. 
+puts zoo[:giraffes]
+# Write code that adds an animal to the zoo hash.
 # Then, print the updated hash:
 # YOUR CODE HERE
-
+zoo[:rhynos] = 4
+puts zoo
 
 #-------------------
 # Part 2: Email
@@ -38,18 +39,19 @@ p zoo
 
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
-
+email_parts = {subject: "Greetings", intro: "Hello", body: "Nice to meet you",
+sign_off: "Cheers", attachments: false}
 # Write code that prints your email hash to the terminal.
+puts email_parts
 
-
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
 # YOUR CODE HERE
-
-# Write code that prints all of the 'values' of the email hash 
+puts email_parts.keys
+# Write code that prints all of the 'values' of the email hash
 # you created above:
 # YOUR CODE HERE
-
+puts email_parts.values
 
 #-------------------
 # Part 3: Many Emails - OPTIONAL EXTENSION
@@ -64,7 +66,7 @@ p zoo
 # posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 
-# Frankly, that was a very simplified version of the Array the Instagram developers have 
+# Frankly, that was a very simplified version of the Array the Instagram developers have
 # written and work with. Still probably slightly simplified as we don't know what their code
 # actually looks like, but it may look more like this:
 
@@ -76,7 +78,7 @@ posts = [
     'timestamp' => "4:37 PM August 13, 2019",
     'number_likes' => 0,
     'comments' => []
-  }, 
+  },
   {
     'image_src' => "./images/holiday-party.png",
     'caption' => "What a great holiday party omg",
@@ -90,12 +92,36 @@ puts posts
 puts posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in an 
-# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in an
+# Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same
 # key-value pairs you used in your email Object above.
 # Then, log the email Array to the console.
+email_formula = [
+  {
+    subject: "Important!!!",
+    intro: "You have won 1 million dollars!!!",
+    body: "Just send us your bank routing and account number in this email!!!",
+    sign_off:"Good fortune!!!",
+    attachments: false
+  },
+  {
+    subject: "Homework",
+    intro: "Hello",
+    body: "Here is my homwwork assignment fot the week.",
+    sign_off: "Thanks",
+    attachments: true
+  },
+  {
+    subject: "Cool link",
+    intro: "heyo",
+    body: "Check out this cool website I found...",
+    sign_off: "See ya!",
+    attachments: false
+  }
+]
+puts email_formula
